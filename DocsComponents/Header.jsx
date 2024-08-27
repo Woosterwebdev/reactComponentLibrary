6fd339f86016c81c00e8c502b6e5fcf06b849e1e
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-27 08:52:42 -0700

    troubleshooting

--- DocsComponents/Header.jsx
+++ DocsComponents/Header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
     return(
         <div className="header">
-            <img src={process.env.PUBLIC_URL + '/react-icon.png'} alt="React Logo." />
+            <img src="/public/react-icon.png" alt="React Logo." />
             <h1 className="title m-0">React Component Library</h1>
         </div>
     )
