7df49cbc45ff0279d11beb356a7c8b9fda3149bc
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-27 17:11:05 -0700

    oops

--- DocsComponents/TestimonialWithImage.jsx
+++ DocsComponents/TestimonialWithImage.jsx
@@ -11,7 +11,7 @@ export default function TestimonialWithImage() {
                     <TestimonialWithImg.Quote>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. 
                     </TestimonialWithImg.Quote>
-                <TestimonialWithImg.Author author="May Andersons" role="Workcation, CTO" />
+                    <TestimonialWithImg.Author author="May Andersons" role="Workcation, CTO" />
                 </TestimonialWithImg>
             </div>
             <h3>Syntax</h3>
